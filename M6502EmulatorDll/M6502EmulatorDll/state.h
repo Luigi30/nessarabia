df3f720aeb8b4f6689d1e838f09ac5c2cc8af13a
Luigi Thirty <luigi30@gmail.com>
2015-12-13 11:49:01 -0500

    initial commit

---
+++ M6502EmulatorDll/M6502EmulatorDll/state.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "stdafx.h"
+#include "CPU.h"
+#include "mc6821.h"
+#include "s2513.h"
+
+class EmulatorState {
+public:
+	enum COMPUTER_CONFIG { NES };
+	static EmulatorState* Instance();
+	CPU processor = CPU();
+	
+	void init();
+	COMPUTER_CONFIG EmulatorState::getConfigurationType();
+
+private:
+	EmulatorState() {};
+	EmulatorState(EmulatorState const&) {};
+	EmulatorState& operator=(EmulatorState const&) {};
+	static EmulatorState* m_pInstance;
+	COMPUTER_CONFIG config = NES;
+};
