91dceda279d54c6b1228c440e02bb9301146142f
Luigi Thirty <luigi30@gmail.com>
2015-12-18 22:32:05 -0500

    added cp;u support files

--- nessarabia/MainWindow.xaml.cs
+++ nessarabia/MainWindow.xaml.cs
@@ -13,6 +13,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+/*
+- http://web.textfiles.com/games/ppu.txt
+
+- One scanline is exactly 1364 cycles long. In comparison to the CPU's speed, one scanline is 1364/12 CPU cycles long.
+
+- One frame is exactly 357368 cycles long, or exactly 262 scanlines long.
+
+*/
+
 namespace nessarabia
 {
     /// <summary>
